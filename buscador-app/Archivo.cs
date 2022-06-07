69ebeba9f69c4438c8c0610986d2f4dbfce8d82b
bobadillajuan <juampi348@gmail.com>
2022-06-07 15:59:11 -0300

    Punto 1 terminado

---
+++ buscador-app/Archivo.cs
@@ -0,0 +1,8 @@
+// See https://aka.ms/new-console-template for more information
+public class Archivo{
+    int Registro;
+    String Nombre;
+    String path;
+    String Extension;
+}
+
---
+++ buscador-app/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+var ListaDeArchivos = new List<string>();
+Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Ingrese el path que desea enumerar: ");
+string path = Console.ReadLine();
+string DirecArchivo = @"C:\Users\Alumno\Desktop\tp8diaz\tp08-2022-diazagustin99\buscador-app\index.csv";
+ListaDeArchivos.Add("Orden;Nombre;Extension");
+if (Directory.Exists(path))
+{
+    var Archivos = Directory.GetFiles(path);
+
+    for (int i = 0; i < Archivos.Count(); i++)
+    {
+        ListaDeArchivos.Add(i + ";" + Path.GetFileNameWithoutExtension(Archivos[i]) + ";" + Path.GetExtension(Archivos[i]));
+    }
+
+    foreach (var item in ListaDeArchivos)
+    {
+        System.Console.WriteLine(item.ToString());
+    }
+File.WriteAllLines(DirecArchivo, ListaDeArchivos);
+
+}
+
